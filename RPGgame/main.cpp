afc770e7f870ac8890270914f5dffe3ce21ed49b
Paarth Pugalia <paarth.pugalia1102@gmail.com>
2024-03-20 23:41:03 +0530

    Added Shooting Bullets with a Mouse  Click
    -Added skeleton spritesheet
    -Added the ability to shoot for the player

--- RPGgame/main.cpp
+++ RPGgame/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <vector>
 #include <SFML/Graphics.hpp>
 
 using namespace std;
 using namespace sf;
+
+sf::Vector2f NormalizeVector(sf::Vector2f vector) {
+
+    float magnitude = sqrt(vector.x* vector.x + vector.y * vector.y);
+
+    sf::Vector2f normalizedVector;
+    normalizedVector.x = vector.x / magnitude;
+    normalizedVector.y = vector.y / magnitude;
+
+    return normalizedVector;
+}
 
 int main() {   
     //-------------------------Initialize-------------------
@@ -11,23 +23,48 @@ int main() {
     settings.antialiasingLevel = 8;
 
     //creating a SFML window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "RPG Game", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "RPG Game", sf::Style::Default, settings);
 
     //-------------------------Initialize-------------------
     
     //-------------------------Load-------------------
-     
+    //-------------------------Skeleton-------------------
+    sf::Texture skeletonTexture;
+    sf::Sprite skeletonSprite;
+    skeletonTexture.loadFromFile("assets/skeleton/textures/spriteSheet.png");
+    skeletonSprite.setTexture(skeletonTexture);
+    skeletonSprite.scale(sf::Vector2f(3, 3));
+    int XSkeletonIndex = 2;
+    int YSkeletonIndex = 3;
+    skeletonSprite.setTextureRect(sf::IntRect(XSkeletonIndex * 64, YSkeletonIndex * 64, 64, 64));
+    skeletonSprite.setPosition(sf::Vector2f(180, 800));
+    //-------------------------Skeleton-------------------
+    
+    //-------------------------Player-------------------
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
 
     playerTexture.loadFromFile("assets/player/textures/spriteSheet.png");
     playerSprite.setTexture(playerTexture);
     playerSprite.scale(sf::Vector2f(3, 3));
+    playerSprite.setPosition(sf::Vector2f(0, 0));
 
-    int XIndex = 0;
-    int YIndex = 0;
-    playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
+    int XPlayerIndex = 0;
+    int YPlayerIndex = 0;
+    playerSprite.setTextureRect(sf::IntRect(XPlayerIndex * 64, YPlayerIndex * 64, 64, 64));
+    //-------------------------Player-------------------
+
+    vector<sf::RectangleShape> bullets;
+
+    float bulletSpeed = 0.5f;
+    //bullet.setPosition(playerSprite.getPosition());
+    
     //-------------------------Load-------------------
+    
+    //-------------------------Calculating direction of bullet-------------------
+    
+
+    //-------------------------Calculating direction of bullet-------------------
     
     //creating a game loop
     while (window.isOpen()) {
@@ -43,6 +80,7 @@ int main() {
                 window.close();
             }
         }
+
         
         sf::Vector2f position = playerSprite.getPosition();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
@@ -64,11 +102,29 @@ int main() {
 
             playerSprite.setPosition(position + sf::Vector2f(0, 1));
         }
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+
+            bullets.push_back(sf::RectangleShape(sf::Vector2f(45, 15)));
+            bullets[bullets.size() - 1].setPosition(playerSprite.getPosition());
+        }
+
+        for (size_t i = 0; i < bullets.size(); i++) {
+            
+            sf::Vector2f bulletDirection;
+            bulletDirection = skeletonSprite.getPosition() - bullets[i].getPosition();
+            bulletDirection = NormalizeVector(bulletDirection) * bulletSpeed;
+            bullets[i].setPosition(bullets[i].getPosition() + bulletDirection);
+        }
         //-------------------------Update-------------------
 
         //-------------------------Draw-------------------
         window.clear(sf::Color::Black);
         window.draw(playerSprite);
+        window.draw(skeletonSprite);
+        for (size_t i = 0; i < bullets.size(); ++i) {
+            window.draw(bullets[i]);
+        }
         window.display();
         //-------------------------Draw-------------------
     }
